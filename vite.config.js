403d201c14f602eb94561c98092e24b25f3607b3
IshakDJ <ishakdjaber17@gmail.com>
2025-04-03 20:16:44 +0100

    NE

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "./", // Set "website" as the working directory
-  build: { // Output everything directly in "dist/"
+  root: "", // Set "website" as the working directory
+  build: { 
     rollupOptions: {
       input: "website/website.html", // Ensure Vite treats website.html as the entry point
     },
